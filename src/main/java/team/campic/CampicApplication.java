3490d636345883000b75fd8c4690bb209a1bd056
Junmo <hongjm1022@gmail.com>
2025-03-12 17:16:02 +0900

    Initial setting

---
+++ src/main/java/team/campic/CampicApplication.java
@@ -0,0 +1,14 @@
+package team.campic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class CampicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/team/campic/CampicApplicationTests.java
@@ -0,0 +1,13 @@
+package team.campic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
